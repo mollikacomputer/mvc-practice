335fddef62846e3f1032f51039b9ef2271e55050
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-05 09:44:55 +0600

    static file serve

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('mvc patern practice project')
+    // res.send('mvc patern practice project');
+    res.sendFile(__dirname + '/public/home.html')
 });
 
 app.listen(port, () => {
